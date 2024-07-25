9071a417305030804ed1c62e389dccb5f03acf97
Seto <setoapriawan69@gmail.com>
2024-07-25 20:44:51 +0700

    initial commit

---
+++ app/src/main/java/com/excal/intent/data/Person.kt
@@ -0,0 +1,16 @@
+package com.excal.intent.data
+
+import android.os.Parcel
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Person(
+    val name:String?,
+    val age:Int?,
+    val email:String?,
+    val city:String?
+) : Parcelable
+
+
+
